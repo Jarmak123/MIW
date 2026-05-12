abdd7ad7ba2d46c90fa7efc84bcd8ebe3d0fb918
JRMK <147035765+Jarmak123@users.noreply.github.com>
2026-05-12 22:38:06 +0200

    Add files via upload

--- naive-bayess/main.py
+++ naive-bayess/main.py
@@ -8,8 +8,8 @@ def get_data(name_of_file):
     with open(name_of_file, "r") as f:
         for line in f:
             row = []
-            for x in range(0,len(line.strip().split(','))):
-                if x == len(line.strip().split(','))-1: row.append(line.strip().split(',')[x])
+            for x in range(0, len(line.strip().split(','))):
+                if x == len(line.strip().split(',')) - 1: row.append(line.strip().split(',')[x])
                 else: row.append(float(line.strip().split(',')[x]))
             result.append(row)
 
@@ -19,95 +19,142 @@ def print_data(data):
     for i in range(0, len(data)):
         print(data[i])
 
-def policz_p_od_c(c):
-    slownik={}
-    unikat_klasy=[]
+def priori(c):
+    slownik = {}
+    unikat_klasy = []
     for i in c:
-        if i[-1] not in unikat_klasy: unikat_klasy.append(i)
+        if i[-1] not in unikat_klasy: unikat_klasy.append(i[-1])
 
     for x in unikat_klasy:
-        temp_licznik=0
-        for y in range(0,len(c)):
-            if c[y][-1]==x: temp_licznik+=1
+        temp_licznik = 0
+        for y in range(0, len(c)):
+            if c[y][-1] == x: temp_licznik += 1
 
-        slownik[x] = temp_licznik/len(c)
+        slownik[x] = temp_licznik / len(c)
 
     return slownik
 
-def utworz_stat_klas(c):
-    slownik={}
+def stats_by_class(c):
+    slownik = {}
     unikat_klasy = []
     for i in c:
-        if i[-1] not in unikat_klasy: unikat_klasy.append(i)
+        if i[-1] not in unikat_klasy: unikat_klasy.append(i[-1])
 
-    for x in unikat_klasy:
-        for y in range(0,len(c)):
-            elementy_atrybutu = []
-            if x != c[y][-1]: continue
-            for j in range(0,len(c[y])):
-                for i in (0, len(c)):
-                    elementy_atrybutu.append(c[i][j])
-        slownik[x]=(np.mean(elementy_atrybutu),np.var(elementy_atrybutu))
+    for klasa in unikat_klasy:
+        obiekty_klasy = []
+        for obiekt in c:
+            if obiekt[-1] == klasa: obiekty_klasy.append(obiekt)
+
+        statystyki = []
+        for j in range(len(c[0]) - 1):
+            kolumna = []
+            for obiekt in obiekty_klasy:
+                kolumna.append(obiekt[j])
+            statystyki.append((np.mean(kolumna), np.var(kolumna)))
+
+        slownik[klasa] = statystyki
 
     return slownik
 
 def kross_walidacja(dane):
-    kafelki=[]
-    obecne=[]
+    kafelki = []
+    obecne = []
     temp = []
-    licznik=0
-    while(licznik!=len(dane)):
-        j = random.randint(0,149)
+    licznik = 0
+    while(licznik != len(dane)):
+        j = random.randint(0, 149)
         if j not in obecne:
             obecne.append(j)
-            licznik=licznik+1
+            licznik = licznik + 1
             temp.append(dane[j])
-            if len(temp)==30:
+            if len(temp) == 30:
                 kafelki.append(temp)
-                temp=[]
+                temp = []
 
     return kafelki
 
-def macierz_pomylek(przewidzne,rzecziwiste):
-    #true/false | posivite/negative
-    #przewidzenie | rzeczywistosc
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
+def macierz_pomylek(przewidzane, rzeczywiste, klasy):
+    n = len(klasy)
+    macierz = []
+    for i in range(0, n):
+        linia=[]
+        for j in range(0,n):
+            linia.append(0)
+        macierz.append(linia)
 
-    for i in range(len(rzecziwiste)):
-        r = rzecziwiste[i]
-        p = przewidzne[i]
+    for i in range(len(rzeczywiste)):
+        r = rzeczywiste[i]
+        p = przewidzane[i]
+        if r in klasy and p in klasy:
+            macierz[klasy.index(p)][klasy.index(r)] += 1
 
-        if p == None: continue
-        elif r == 1 and p == 1: tp += 1
-        elif r == 0 and p == 0: tn += 1
-        elif r == 0 and p == 1: fp += 1
-        elif r == 1 and p == 0: fn += 1
-
-
-    accuracy = (tp+tn) / len(rzecziwiste)
+    poprawne = sum(macierz[i][i] for i in range(n))
+    accuracy = poprawne / len(rzeczywiste)
 
     print(f"macierz pomylek:")
-    print(f"              rzeczywiste 0:  rzeczywiste 1:")
-    print(f"przewidziane 0:   {tn}         {fn}")
-    print(f"przewidziane 1:   {fp}         {tp}")
-    print(f"accuracy:{accuracy}")
+    print(f"{"przewidziane \\ rzeczywiste":30}", end="")
+    for k in klasy: print(f"{k:20}", end="")
+    print("")
+    for i in range(n):
+        print(f"{klasy[i]:30}", end="")
+        for j in range(n): print(f"{macierz[i][j]:<20}", end="")
+        print("")
+    print(f"accuracy: {accuracy}")
 
     return accuracy
 
-def policz_dla_kazdego(war, atrybut,srednia):
-    dol_ulamka = math.sqrt(2*math.pi*war**2)
-    do_potegi = -1*((atrybut-srednia)**2/(2*war**2))
-    razem = 1/dol_ulamka * math.e**do_potegi
+def rozklad_normalny(war, atrybut, srednia):
+    dol_ulamka = math.sqrt(2 * math.pi * war)
+    do_potegi = -1 * ((atrybut - srednia) ** 2 / (2 * war))
+    razem = 1 / dol_ulamka * math.e ** do_potegi
     return razem
+
+def naive_bayes(tst, trn):
+    procent_klas = priori(trn)
+    stats = stats_by_class(trn)
+    klasy = list(procent_klas.keys())
+    przewidziane_klasy = []
+    for obiekt in tst:
+        wyniki = {}
+        for klasa in klasy:
+            log_suma = math.log(procent_klas[klasa])
+            for j in range(len(obiekt) - 1):
+                srednia, wariancja = stats[klasa][j]
+                p_ai = rozklad_normalny(wariancja, obiekt[j], srednia)
+                if p_ai > 0:
+                    log_suma += math.log(p_ai)
+                else:
+                    log_suma += -9999999
+
+            wyniki[klasa] = log_suma
+        przewidziana = max(wyniki, key=wyniki.get)
+        przewidziane_klasy.append(przewidziana)
+    return przewidziane_klasy
 
 def main():
     data = get_data("iris.data")
-    print_data(data)
-    kafelki=kross_walidacja(data)
-    print_data(kafelki)
+    kafelki = kross_walidacja(data)
+    klasy = list(set(obiekt[-1] for obiekt in data))
+    ogolne_accuracy = []
 
-    np.log()
+    for numer_kafla in range(5):
+        print(f'\n---------------Iteracja {numer_kafla + 1}-------------------')
+
+        zbior_testowy = kafelki[numer_kafla]
+        zbior_treningowy = []
+
+        for i in range(5):
+            if i != numer_kafla:
+                for obiekt in kafelki[i]:
+                    zbior_treningowy.append(obiekt)
+
+        rzeczywiste = [obiekt[-1] for obiekt in zbior_testowy]
+        przewidziane = naive_bayes(zbior_testowy, zbior_treningowy)
+
+        accuracy = macierz_pomylek(przewidziane, rzeczywiste, klasy)
+        ogolne_accuracy.append(accuracy)
+
+    srednia = sum(ogolne_accuracy) / len(ogolne_accuracy)
+    print(f"\n---------------Srednie accuracy: {srednia}-------------------")
+
 main()
